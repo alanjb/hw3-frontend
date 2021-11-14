5b3269f5befc39c8ebcaccb27dea118bb784e0e1
Alan <boyce.alan15@gmail.com>
2021-11-14 12:30:58 -0500

    adding jenkinsfile, adding required input validation

--- src/app/survey/survey.component.ts
+++ src/app/survey/survey.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { AbstractControl, FormBuilder, FormGroup, Validators,  } from "@angular/forms";
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Survey } from './survey';
 
@@ -10,22 +10,23 @@ import { Survey } from './survey';
 })
 export class SurveyComponent implements OnInit {
   form: FormGroup;
-  serverUrl = "http://ec2-3-143-235-208.us-east-2.compute.amazonaws.com/SWE645SurveyService";
+  submitted = false;
+  serverUrl = "http://ec2-3-143-235-208.us-east-2.compute.amazonaws.com/SWE645SurveyService"; //need to update this!
   surveysEndpoint = "/surveys";
   headers = new HttpHeaders().set('Content-Type', 'application/json');
   Recommend = ["Likely", "Very Likely", "Unlikely"];
 
-  constructor(public fb: FormBuilder, private http: HttpClient) {
+  constructor(private fb: FormBuilder, private http: HttpClient) {
     this.form = this.fb.group({
-      firstName: [''],
-      lastName: [''],
-      street: [''],
-      city: [''],
-      state: [''],
-      zip: [''],
-      tel: [''],
-      email: [''],
-      date: [''],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zip: ['', Validators.required],
+      tel: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      date: ['', Validators.required],
       likeStudents: false,
       likeLocation: false,
       likeCampus: false,
@@ -41,7 +42,13 @@ export class SurveyComponent implements OnInit {
 
   ngOnInit() { }
 
-  submitSurvey() {
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  submitSurvey(): void {
+    this.submitted = true;
+
     const object: Survey = {
       firstName: this.form.get('firstName')?.value,
       lastName: this.form.get('lastName')?.value,
@@ -64,10 +71,18 @@ export class SurveyComponent implements OnInit {
       comments: this.form.get('comments')?.value
     };
 
-    this.http
-      .post(this.serverUrl + this.surveysEndpoint, object, { headers: this.headers })
-      .subscribe(
-      (response) => alert('Success! Survey submitted'),
-      (error) => alert('Error! There was an issue submitting your survey response'))
+    // this.http
+    //   .post(this.serverUrl + this.surveysEndpoint, object, { headers: this.headers })
+    //   .subscribe(
+    //     response => {
+    //       alert('Success! Survey submitted');
+    //       this.onReset();
+    //     },
+    //   (error) => alert('Error! There was an issue submitting your survey response'))
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.form.reset();
   }
 }
